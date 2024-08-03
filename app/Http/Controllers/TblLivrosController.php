e2b6b9e6a6baea1b47b9cd1c7271a3595149bdfc
vitorhaoliveira <vitorhaoliveira123@gmail.com>
2024-08-02 22:53:18 -0300

    new functions

---
+++ app/Http/Controllers/TblLivrosController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+//CRUD 
+class TblLivrosController extends Controller
+{
+
+    //Mostrar todos os registros
+    public function index()
+    {
+
+    }
+
+
+    //mostrar tipo de registro especifico
+    public function show()
+    {
+        
+    }
+
+
+    //Cadastrar registros
+    public function store()
+    {
+
+    }
+
+
+    //Alterar registros
+    public function update()
+    {
+
+    }
+
+
+    //Deletar registros
+    public function destroy()
+    {
+
+    }
+
+}
